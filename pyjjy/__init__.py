c4dabad883f4cd224ae26a66dbdffb0db4d8444a
ehki <0y35.ejiri.vmqewyhw@gmail.com>
2022-02-13 14:13:15 +0900

    Set version to 0.3-dev

--- pyjjy/__init__.py
+++ pyjjy/__init__.py
@@ -1,7 +1,7 @@
 from .pyjjy import JJYsignal, main
 
 __copyright__ = 'Copyright (C) 2022 Haruki EJIRI'
-__version__ = '0.2'
+__version__ = '0.3-dev'
 __license__ = 'MIT'
 __author__ = 'Haruki EJIRI'
 __author_email__ = '0y35.ejiri.vmqewyhw@gmail.com'
